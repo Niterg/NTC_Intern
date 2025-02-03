64113d6ae67cb38a8a04f193d8cac6fc2675562b
P Niraula <83171743+Niterg@users.noreply.github.com>
2025-02-03 16:28:05 +0545

    Update urls.py

--- MapUI/mapui/mapui/urls.py
+++ MapUI/mapui/mapui/urls.py
@@ -1,11 +1,23 @@
-from django.urls import path
-from .views import get_towers
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+"""
+URL configuration for mapui project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    # path("", views.map_search, name="mao_search"),
-    path("", views.show_towers, name="show_towers"),
-    path('get-towers/', views.get_towers, name='get_towers'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include("map_app.urls")),
+]
