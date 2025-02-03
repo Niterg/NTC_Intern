a93a42b92c1d88b740876c1aa520df5683349433
P Niraula <83171743+Niterg@users.noreply.github.com>
2025-02-03 16:28:49 +0545

    updated urls.py with static input

--- MapUI/mapui/map_app/urls.py
+++ MapUI/mapui/map_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import get_towers
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.maps, name="maps"),
-    path("maps_search", views.maps_search, name="maps_search"),
-    path('get-towers/', get_towers, name='get_towers'),
-]
-
-
+    # path("", views.maps, name="maps"),
+    # path("maps_search", views.maps_search, name="maps_search"),
+    path("", views.show_towers, name="show_towers"),
+    path('get-towers/', views.get_towers, name='get_towers'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
