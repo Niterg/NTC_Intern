a1d5d1822379a1091275f60ee7624f2d5d735ffc
P Niraula <83171743+Niterg@users.noreply.github.com>
2025-02-15 16:38:53 +0545

    mapui changes

--- MapUI/mapui/map_app/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 1536a59 (Added authentication)
